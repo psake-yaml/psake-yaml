ca43ee7462ef4415c6f6a55609afa2d15c2be25e
Jozef Izso <jozef.izso@gmail.com>
2016-09-09 20:51:13 +0200

    Fix Framework tests

--- specs/using_framework_should_pass.ps1
+++ specs/using_framework_should_pass.ps1
@@ -1,16 +1,19 @@
-﻿task default -depends FrameworkFunction 
+﻿task default -depends FrameworkFunction
 
 task FrameworkFunction  {
-	AssertFramework '2.0'
-	AssertFramework '3.5'
-	AssertFramework '4.0'
+	AssertFramework '2.0' '2.0.50727.8745' '2.0.50727.8745'
+	AssertFramework '3.5' '3.5.30729.8763' '2.0.50727.8745'
+	AssertFramework '4.0' '4.6.1586.0' '4.0.30319.42000'
 }
 
 function AssertFramework{
 	param(
-		[string]$framework
+		[string]$framework,
+		[string]$engineVersion,
+		[string]$frameworkVersion
 	)
 	Framework $framework
 	$msBuildVersion = msbuild /version
-	Assert ($msBuildVersion[0].ToLower().StartsWith("microsoft (r) build engine version $framework")) '$msBuildVersion does not start with "$framework"'
+	Assert ($msBuildVersion[0].StartsWith("microsoft (r) build engine version $engineVersion", [System.StringComparison]::InvariantCultureIgnoreCase)) '$msBuildVersion does not contain engine version "$engineVersion"'
+	Assert ($msBuildVersion[1].StartsWith("[Microsoft .NET Framework, version $frameworkVersion]", [System.StringComparison]::InvariantCultureIgnoreCase)) '$msBuildVersion does not contains framework version "$framework"'
 }
