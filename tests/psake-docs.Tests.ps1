855534aaaf0237e12e29729492364f03529cf5ac
Jozef Izso <jozef.izso@gmail.com>
2016-09-10 12:04:23 +0200

    Add Pester test for detailed docs

--- tests/psake-docs.Tests.ps1
+++ tests/psake-docs.Tests.ps1
@@ -38,6 +38,50 @@ UnitTests        ut
 
 "@ -f $psake.version
 
+        $expectedDetailedDoc = @"
+
+
+Name        : Compile
+Alias       : 
+Description : 
+Depends On  : CompileSolutionA, CompileSolutionB
+Default     : True
+
+Name        : CompileSolutionA
+Alias       : 
+Description : Compiles solution A
+Depends On  : 
+Default     : 
+
+Name        : CompileSolutionB
+Alias       : 
+Description : 
+Depends On  : 
+Default     : 
+
+Name        : IntegrationTests
+Alias       : 
+Description : 
+Depends On  : 
+Default     : 
+
+Name        : Test
+Alias       : 
+Description : 
+Depends On  : UnitTests, IntegrationTests
+Default     : True
+
+Name        : UnitTests
+Alias       : ut
+Description : 
+Depends On  : 
+Default     : 
+
+
+
+
+"@
+
         It "should return expected documentation without logo" {
             $doc = Invoke-Psake "$here\specs\nested\docs.ps1" -docs -nologo | Out-String
 
@@ -48,6 +92,12 @@ UnitTests        ut
             $doc = Invoke-Psake "$here\specs\nested\docs.ps1" -docs | Out-String
 
             $doc | Should BeExactly $expectedLogoDoc
+        }
+
+        It "should return expected detailed documentation without logo" {
+            $doc = Invoke-Psake "$here\specs\nested\docs.ps1" -detailedDocs -nologo | Out-String
+
+            $doc | Should BeExactly $expectedDetailedDoc
         }
     }
 }
